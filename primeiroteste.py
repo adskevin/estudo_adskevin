1bfbf5d3ea16a8113f0c38e352d4d52697cb9783
adskevin <kevin.souza.94@gmail.com>
2018-08-18 19:39:15 -0300

    Teste de loop

--- primeiroteste.py
+++ primeiroteste.py
@@ -1,1 +1,10 @@
-print("teste")
+contador = int(0)
+valor = 0
+sair = True
+while sair:
+    valor = int(input())
+    if valor>60:
+        sair = False
+    contador += 1
+    print(contador)
+print("Saiu.")
